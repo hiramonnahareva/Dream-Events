e883a66a005028beea162a96f8a7b45629e7718b
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-21 20:56:28 +0600

    add contact page

--- src/Component/Service/ComingEvents/SingleEvent.js
+++ src/Component/Service/ComingEvents/SingleEvent.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const SingleEvent = ({id, img,  title}) => {
  
   return (
-    //   <div id="item1" className="carousel-item w-96 gap-6">
     <div id={`item${id}`} className="carousel-item w-96 gap-6 mx-4">
       <div><img src={img} className="w-50" />
         <div className="flex justify-center mt-[-6%]">
@@ -12,17 +11,6 @@ const SingleEvent = ({id, img,  title}) => {
         </div>
         <h2 className='text-2xl text-center mt-4 cursor-pointer hover:text-purple-600'><Link to={`/service/${id}`}>{title}</Link></h2></div>
     </div>
-    //   {/* <div id="item2" className="carousel-item w-[800px] mx-20">
-    //     <img src="http://demo.ovathemes.com/dvents/wp-content/uploads/2017/06/logo-1.png" className="w-full" />
-    //   </div> 
-    //   <div id="item3" className="carousel-item w-96">
-    //     <img src="http://demo.ovathemes.com/dvents/wp-content/uploads/2017/06/logo-1.png" className="w-full" />
-    //   </div> 
-    //   <div id="item4" className="carousel-item w-96">
-    //     <img src="http://demo.ovathemes.com/dvents/wp-content/uploads/2017/06/logo-1.png" className="w-full" />
-    //   </div> */}
-
-    //         // </div>
   );
 };
 
