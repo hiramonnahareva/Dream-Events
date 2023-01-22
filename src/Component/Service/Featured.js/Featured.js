afe8c57e5c995c39dac618e74d3e246994c7efd2
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-22 16:28:18 +0600

    complete feature section

--- src/Component/Service/Featured.js/Featured.js
+++ src/Component/Service/Featured.js/Featured.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdOutlineEmojiEvents } from 'react-icons/md';
 import { RiCake2Line } from 'react-icons/ri';
 import { TbMicrophone2 } from 'react-icons/tb';
 import image from '../../../Images/Group 21 (4).png';
@@ -19,8 +20,8 @@ const Featured = () => {
         },
         {
             _id: 3,
-            icon: <RiCake2Line className='text-4xl'/>,
-            title: 'Services Featured',
+            icon: <MdOutlineEmojiEvents className='text-4xl'/>,
+            title: 'Award Ceremony',
             param: 'Sit amet consectetur elit sed lusm tempor incidant temdore ut labore dolore lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore.'
         },
     ]
@@ -30,21 +31,21 @@ const Featured = () => {
         <img src={image} className='relative' alt="" />
         <div className='absolute w-full px top-20 flex justify-center lg:flex-row flex-col'>
             <div className=''>
-            <h2 className='text-white text-xl'>With a full range of Event Planning Services, our Clients have Successful & Prosperous Events!</h2>
-            <h2 className='text-white'>We make your events smart & impactful by personalised event management services.</h2>
+            <h2 className='text-white text-xl mt-2'>With a full range of Event Planning Services, our Clients have Successful & Prosperous Events!</h2>
+            <h2 className='text-gray-300'>We make your events smart & impactful by personalised event management services.</h2>
             </div>
         </div>
-        <div className='absolute w-full px-8 lg:px-36 top-72 flex justify-center gap-40 items-center lg:flex-row flex-col'>
-            <div className=''>
-            <h2 className='text-white text-xl'>Services Featured</h2>
-            <h2 className='text-white'>We make your events smart & impactful by personalised event management services.</h2>
-            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ex ea consequat duis aute irure dolor in reprehenderit in voluptate labore et dolore.</p>
+        <div className='absolute w-full px-8 lg:px-36 top-72 flex justify-center gap-40  lg:flex-row flex-col'>
+            <div className='text-white'>
+            <h2 className='text-4xl font-semibold mb-4'><span className='text-purple-600'>Services </span> Featured</h2>
+            <h2 className='mb-6 text-[18px]'>We make your events smart & impactful by personalised event management services.</h2>
+            <p className='text-gray-100 text-[16px]'>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ex ea consequat duis aute irure dolor in reprehenderit in voluptate labore et dolore.</p>
             </div>
             <div className=''>
                {
                 featured.map(item => 
-                    <div className="">
-               <div className="flex items-center gap-6 text-white text-2xl">
+                    <div className="mb-6">
+               <div className="flex items-center gap-6 text-white text-2xl mb-2">
                 <div className='flex items-center gap-2'>
                 {item.icon}
                <div className='border-b-[3px] w-[20px] border-purple-600'></div>
