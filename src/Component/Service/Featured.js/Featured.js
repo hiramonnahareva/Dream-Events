86227e43bfbafa83ca0b8921e8068d527e2ab728
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-22 16:03:46 +0600

    add featued content

--- src/Component/Service/Featured.js/Featured.js
+++ src/Component/Service/Featured.js/Featured.js
@@ -1,15 +1,61 @@
 import React from 'react';
-import image from '../../../Images/Group 21.png'
+import { RiCake2Line } from 'react-icons/ri';
+import { TbMicrophone2 } from 'react-icons/tb';
+import image from '../../../Images/Group 21 (4).png';
 
 const Featured = () => {
+    const featured = [
+        {
+            _id: 1,
+            icon: <RiCake2Line className='text-4xl'/>,
+            title: 'Birthday Parties',
+            param: 'Sit amet consectetur elit sed lusm tempor incidant temdore ut labore dolore  lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore.'
+        },
+        {
+            _id: 2,
+            icon: <TbMicrophone2 className='text-4xl'/>,
+            title: 'Corporate Seminars',
+            param: 'Sit amet consectetur elit sed lusm tempor incidant temdore ut labore dolore lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore.'
+        },
+        {
+            _id: 3,
+            icon: <RiCake2Line className='text-4xl'/>,
+            title: 'Services Featured',
+            param: 'Sit amet consectetur elit sed lusm tempor incidant temdore ut labore dolore lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore.'
+        },
+    ]
     return (
        
         <div className='relative'>
         <img src={image} className='relative' alt="" />
-        <div className='absolute top-0 flex justify-center items-center lg:flex-row flex-col'>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, nihil.</h2>
-            
-
+        <div className='absolute w-full px top-20 flex justify-center lg:flex-row flex-col'>
+            <div className=''>
+            <h2 className='text-white text-xl'>With a full range of Event Planning Services, our Clients have Successful & Prosperous Events!</h2>
+            <h2 className='text-white'>We make your events smart & impactful by personalised event management services.</h2>
+            </div>
+        </div>
+        <div className='absolute w-full px-8 lg:px-36 top-72 flex justify-center gap-40 items-center lg:flex-row flex-col'>
+            <div className=''>
+            <h2 className='text-white text-xl'>Services Featured</h2>
+            <h2 className='text-white'>We make your events smart & impactful by personalised event management services.</h2>
+            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ex ea consequat duis aute irure dolor in reprehenderit in voluptate labore et dolore.</p>
+            </div>
+            <div className=''>
+               {
+                featured.map(item => 
+                    <div className="">
+               <div className="flex items-center gap-6 text-white text-2xl">
+                <div className='flex items-center gap-2'>
+                {item.icon}
+               <div className='border-b-[3px] w-[20px] border-purple-600'></div>
+                </div>
+            <h2 className=''>{item.title}</h2>
+               </div>
+               <p className='text-white'>Sit amet consectetur elit sed lusm tempor incidant temdore ut labore dolore lorem ipsum dolor sit amet consectetur adipisicing elit sed do , eiusmod tempor incididunt ut labore et dolore.</p>
+               </div>
+                    )
+               }
+            </div>
         </div>
          </div>
     );
