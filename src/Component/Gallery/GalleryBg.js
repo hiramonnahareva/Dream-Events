dee046791e92c4b5c8814b7e1d6ce6917c198317
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-02-11 00:41:49 +0600

    change width

--- src/Component/Gallery/GalleryBg.js
+++ src/Component/Gallery/GalleryBg.js
@@ -6,9 +6,9 @@ const GalleryBg = ({item}) => {
     return (
             <div  style={{
             backgroundImage: `url(${img})`,
-          }} className={`rounded-tl-3xl sticky rounded-br-3xl top-0 bg-scroll  h-[280px] bg-center bg-primary bg-cover`}>
+          }} className={`rounded-tl-3xl sticky rounded-br-3xl top-0 bg-scroll h-[280px] bg-center bg-primary bg-cover`}>
             <div className=' absolute top-0'>
-                <div className={`rounded-tl-3xl rounded-br-3xl ${bg} lg:w-[699px] w-[100%] h-[280px] bg-center bg-cover`}></div>
+                <div className={`rounded-tl-3xl rounded-br-3xl ${bg} lg:w-[710px] w-[100%] h-[280px] bg-center bg-cover`}></div>
             <div className='absolute text-center lg:top-14 top-5 w-full text-white'>
                 <h2 className='text-3xl font-semibold'>{title}</h2>
             <p className='mt-3 mb-9'>{text}</p>
